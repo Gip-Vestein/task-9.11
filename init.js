fd9e5b773c935cecbb0719ba62334159c65c6641
Grebennikov Leonid <101121332+Gip-Vestein@users.noreply.github.com>
2022-05-26 14:01:23 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,12 +1,12 @@
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
-    document.getElementById('genderOtput').innerText=initPerson.gender;
-    document.getElementById('firstNameOutput').innerText = initPerson.firstName[0];
+    document.getElementById('genderOutput').innerText=initPerson.gender;
+    document.getElementById('firstNameOutput').innerText = initPerson.name[0];
     document.getElementById('surnameOutput').innerText = initPerson.name[1];
     document.getElementById('patronymicOutput').innerText = initPerson.name[2];
     document.getElementById('professionOutput').innerText = initPerson.name[3];
-    document.getElementById('birthYearOutput').innerText=initPerson.birthYear;
+    document.getElementById('birthDateOutput').innerText=initPerson.birthDate;
 };
 
 document.getElementById('reboot').addEventListener('click',function(){
@@ -14,11 +14,10 @@ document.getElementById('reboot').addEventListener('click',function(){
 });
 
 document.getElementById('clear').addEventListener('click',function () {
-    document.getElementById('genderOutput').innerText= 'Пол';
+    document.getElementById('genderOutput').innerText= 'пол';
     document.getElementById('firstNameOutput').innerText = '';
     document.getElementById('surnameOutput').innerText = '';
     document.getElementById('patronymicOutput').innerText = '';
-    document.getElementById('professionOutput').innerText = 'Профессия';
-    document.getElementById('birthYearOutput').innerText= 'Дата рождения';
+    document.getElementById('professionOutput').innerText = 'профессия';
+    document.getElementById('birthDateOutput').innerText= 'дата рождения';
 })
-
